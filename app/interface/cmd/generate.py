1f4f854c7f3f980b2e788a413ec462d34d4c7988
junhodo <bale.do@buzzvil.com>
2021-08-17 02:27:56 +0900

    add mvp

--- app/interface/cmd/generate.py
+++ app/interface/cmd/generate.py
@@ -1,13 +1,21 @@
 import click
+import googletrans
 
+from app.application.usecase.generate import GenerateUseCase
+from app.infra.adapter.google_translation.google_traslator import GoogleTranslator
+from app.infra.repository.page_repository import PageRepository
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 @click.option('--path', type=str, required=True)
-def generate(path :str):
-    print("implement me")
+@click.option('--src_lang', type=str, required=True)
+@click.option('--dest_lang', type=str, required=True)
+def generateFile(path: str, src_lang: str, dest_lang: str):
+    generate_usecase = GenerateUseCase(translator=GoogleTranslator(), page_repository=PageRepository())
+    generate_usecase.generate(path=path, src_lang=src_lang, dest_lang=dest_lang)
 
 if __name__ == '__main__':
-    generate()
+    generateFile()
